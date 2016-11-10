0c9111c9d84c54ca998be5338f1f85b1f9094280
maoge <sun409377708@163.com>
2016-11-10 09:36:15 +0800

    创建项目, 忽略pods文件
    Signed-off-by: Jinx-loli <645643692@qq.com>

---
+++ SinaOCPractice/SinaOCPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaOCPractice
+//
+//  Created by maoge on 16/11/10.
+//  Copyright © 2016年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinaOCPractice/SinaOCPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinaOCPractice
+//
+//  Created by maoge on 16/11/10.
+//  Copyright © 2016年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
